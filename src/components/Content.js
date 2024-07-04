566d5e803b678a2f3d70acded6157d38d55dae7d
Saul <estrellasoscuras1984@gmail.com>
2024-07-04 17:00:24 -0300

    first commit

---
+++ src/components/Content.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Content.css';
+
+const Content = () => {
+  return (
+    <main className="content">
+      <div className="box">Box 1</div>
+      <div className="box">Box 2</div>
+      <div className="box">Box 3</div>
+      <div className="box">Box 4</div>      
+    </main>
+  );
+};
+
+export default Content;
